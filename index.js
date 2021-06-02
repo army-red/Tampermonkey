476bac07c500ecaf3c0edf2c4d0b1ee54d5b4c99
吃小米长大的枪 <863166709@qq.com>
2021-06-02 21:41:02 +0800

    TextBlocker

---
+++ index.js
@@ -0,0 +1,13 @@
+// const saveToStorage = function() {
+//     alert("※上传成功ヽ(･ω･´ﾒ)※ ");
+//     localStorage.setItem("testForCSV", text);
+// }
+
+// const loadFromLocalStorage = function() {
+//     alert(localStorage["testForCSV"]);
+// }
+
+// const removeLocalStorage = function() {
+//     localStorage.removeItem("testForCSV");
+
+// }
